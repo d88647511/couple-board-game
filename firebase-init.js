1c97ed98600542e65c4e20406b9567034bd38dcd
d88647511 <d88647511@gmail.com>
2026-07-22 23:35:58 +0800

    444

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,19 @@
-alert("firebase-init 有執行");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBg-YvvfUNvJNDYlzyd2bA0MuWXotvz5sg",
+  authDomain: "couple-board-game.firebaseapp.com",
+  databaseURL: "https://couple-board-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "couple-board-game",
+  storageBucket: "couple-board-game.firebasestorage.app",
+  messagingSenderId: "133501507868",
+  appId: "1:133501507868:web:17396fe377a37fe83302d9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.db = db;
+
+console.log("✅ Firebase 已連線");
