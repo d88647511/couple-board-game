e5efcc43f1e835d46c6502c4a4f5a740538f3484
d88647511 <d88647511@gmail.com>
2026-07-22 23:52:40 +0800

    11111

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBg-YvvfUNvJNDYlzyd2bA0MuWXotvz5sg",
+  authDomain: "couple-board-game.firebaseapp.com",
+  databaseURL: "https://couple-board-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "couple-board-game",
+  storageBucket: "couple-board-game.firebasestorage.app",
+  messagingSenderId: "133501507868",
+  appId: "1:133501507868:web:17396fe377a37fe83302d9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+console.log("✅ Firebase Ready");
+// 監聽手機是否要求擲骰
+onValue(ref(db, "remote/roll"), (snapshot) => {
+
+    if (snapshot.val() === true) {
+
+        console.log("📱 手機要求擲骰");
+
+        document.getElementById("rollBtn").click();
+
+    }
+
+});
 // ===============================
 // 夫妻人生旅程 Wedding Edition
 // script.js (Part 1)
